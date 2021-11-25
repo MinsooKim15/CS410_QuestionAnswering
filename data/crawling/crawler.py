0fb555c16ebb560864c550dbaab1f90c2a5b2c23
MinSoo Kim <37958640+MinsooKim15@users.noreply.github.com>
2021-11-25 16:48:57 +0900

    Update data_1125 and fixed crawler

--- data/crawling/crawler.py
+++ data/crawling/crawler.py
@@ -13,10 +13,12 @@ import json
 
 # user id and password for selenium log in to campuswire
 
-USER_ID = ""
-USER_PASSWORD = ""
-FILENAME = ""
-CHROME_DRIVER_PATH = ""
+USER_ID = "minsoo4@illinois.edu"
+USER_PASSWORD = "rkdghkeh95"
+FILENAME = "data_1125.json"
+CHROME_DRIVER_PATH = "/Users/minsookim/Downloads/chromedriver"
+MINIMUM_POST_ID = 2
+MAXIMUM_POST_ID = 1750
 def check_exists_by_selector(driver, xpath):
     try:
         driver.find_element_by_css_selector(xpath)
@@ -59,60 +61,37 @@ def getSubReplyList(element):
 
 
 def getPostItem(id, driver):
-    # print("제목")
-    # print(driver.find_element_by_tag_name("h3").text)
-    #
-    # print("본문")
-    # print(driver.find_element_by_class_name("post-body").text)
-    # # print("post-category")
-    # # print(driver.find_element_by_class_name("category-5").text)
-    # print("like")
-    # print(driver.find_element_by_class_name("post-like").find_element_by_class_name("counter").text)
-    # print("comment")
-    # print(driver.find_element_by_class_name("post-comments-count").find_element_by_tag_name("span").text)
-    # print("view-count")
-    # print(driver.find_element_by_class_name("post-views-count").find_element_by_tag_name("span").text)
-    # print("unique-view-count")
-    # print(driver.find_element_by_class_name("unique-views-count-wrap").find_element_by_tag_name("span").text)
+    postUserName = driver.find_element_by_class_name("user--name").find_element_by_tag_name("h6").text
+    postUserType = 2
 
-    userName = driver.find_element_by_class_name("user--name").find_element_by_tag_name("h6").text
-    userType = 2
-    # print("userType")
     if check_exists_by_selector(driver, '.ta-badge'):
-        userType = 1
+        postUserType = 1
     if check_exists_by_selector(driver, '.instructor-badge'):
-        userType = 0
-
-    title = driver.find_element_by_tag_name("h3").text
-    content = driver.find_element_by_class_name("post-body").text
-    likeCount = driver.find_element_by_class_name("post-like").find_element_by_class_name("counter").text
-    commentCount = driver.find_element_by_class_name("post-comments-count").find_element_by_tag_name("span").text
-    viewCount = driver.find_element_by_class_name("post-views-count").find_element_by_tag_name("span").text
-    uniqueViewCount = driver.find_element_by_class_name("unique-views-count-wrap").find_element_by_tag_name("span").text
+        postUserType = 0
+    title = driver.find_element_by_class_name("post-view").find_element_by_class_name("post-title").find_element_by_tag_name("h3").text
+    postContent = driver.find_element_by_class_name("post-view").find_element_by_class_name("post-body").text
+    likeCount = driver.find_element_by_class_name("post-view").find_element_by_class_name("post-like").find_element_by_class_name("counter").text
+    commentCount = driver.find_element_by_class_name("post-view").find_element_by_class_name("post-comments-count").find_element_by_tag_name("span").text
+    viewCount = driver.find_element_by_class_name("post-view").find_element_by_class_name("post-views-count").find_element_by_tag_name("span").text
+    uniqueViewCount = driver.find_element_by_class_name("post-view").find_element_by_class_name("unique-views-count-wrap").find_element_by_tag_name("span").text
     topAnswerList = driver.find_elements_by_class_name("top-answer")
     replyList = []
     for item in topAnswerList:
         # print("userName")
         # print(item.find_element_by_class_name("user--name").find_element_by_tag_name("h6").text)
-        userName = item.find_element_by_class_name("user--name").find_element_by_tag_name("h6").text
-        userType = 2
-        # print("userType")
+        replyUserName = item.find_element_by_class_name("user--name").find_element_by_tag_name("h6").text
+        replyUserType = 2
         if check_exists_by_selector(driver, '.ta-badge'):
-            userType = 1
+            replyUserType = 1
         if check_exists_by_selector(driver, '.instructor-badge'):
-            userType = 0
-        # print("upvote")
-        # print(item.find_element_by_class_name("vote-counter").find_element_by_class_name("badge").text)
+            replyUserType = 0
         upvote = int(item.find_element_by_class_name("vote-counter").find_element_by_class_name("badge").text)
-        #
-        # print("본문")
-        # print(item.find_element_by_class_name("post-body").text)
-        content = item.find_element_by_class_name("post-body").text
+        replyContent = item.find_element_by_class_name("post-body").text
         subReplyList = getSubReplyList(item)
         reply = Reply.fromCrawl(
-            userName=userName,
-            userType=userType,
-            content=content,
+            userName= replyUserName,
+            userType= replyUserType,
+            content= replyContent,
             subReplyList=subReplyList,
             replyType=0
         )
@@ -120,9 +99,9 @@ def getPostItem(id, driver):
     article = Article.fromCrawl(
         id = id,
         category = "post",
-        userName = userName,
-        userType = userType,
-        content = content,
+        userName = postUserName,
+        userType = postUserType,
+        content = postContent,
         title = title,
         inLinkId = "",
         replyList =replyList,
@@ -134,22 +113,23 @@ def getPostItem(id, driver):
     return article
 
 def crawl_caampuswire():
+    # 브라우저 설정 및 호출
     url = 'https://campuswire.com/signin'
-
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=chrome_options)
     driver.get(url)
+    # 로그인 후 화면 전환 대기
     body = driver.find_elements_by_class_name("form-control")
     body[0].send_keys(USER_ID)
     body[1].send_keys(USER_PASSWORD)
     driver.find_element_by_class_name("btn").click()
-    # driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     time.sleep(3)
+    # 로그인 완료 후 피드 이동
     base_url = "https://campuswire.com/c/G0A3AA370/feed/"
     articleList = []
-    for i in range(4, 1350):
+    for i in range(MINIMUM_POST_ID, MAXIMUM_POST_ID):
         url = base_url + str(i)
         driver.get(url)
         time.sleep(3)
